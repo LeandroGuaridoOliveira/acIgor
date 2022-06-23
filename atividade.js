d75df8d69d8f1d7603063c2240727ad64e611fc6
LeandroGuaridoOliveira <84139383+LeandroGuaridoOliveira@users.noreply.github.com>
2022-06-23 13:12:48 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,30 @@
+
+class lanches{
+
+    lanche(carne, queijo, alface,molho, cebola, picles, pao, frango){
+
+        this.carne = carne;
+        this.queijo = queijo;
+        this.alface = alface;
+        this.molho = molho;
+        this.cebola = cebola;
+        this.picles = picles;
+        this.pao = pao;
+        this.frango = frango;
+        this.fruta = fruta;
+        this.batataPequena = batataPequena;
+        this.batatamedia = batatamedia;
+        this.batatagrande = batatagrande;
+        this.bebida = bebida;
+
+    }
+}
+    class McLancheFeliz{
+        pao; carne; queijo; batataPequena; fruta;    
+    }
+    class BigLanche{
+        pao; carne; carne; queijo;  molho; cebola; picles; batatamedia; bebida;
+    }
+    class McRen{
+        pao; frango; carne; alface; cebola; picles; batatamedia; bebida;
+    }
